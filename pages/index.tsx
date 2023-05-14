8e9cf537f6c31d809e19d533ba356e053ef4c250
Tai Nguyen Trong <trongtaind37@gmail.com>
2023-05-14 16:47:26 +0700

    chore: simplify content

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,26 +8,26 @@ interface HomeProps {
 }
 
 const Home: NextPage<HomeProps> = ({ posts }) => {
-  console.log('🚀 ~ file: index.tsx:11 ~ posts', posts);
   return (
     <div className='mx-auto max-w-3xl prose'>
       <p></p>
       <h1>Hi!, I&apos;m Tai</h1>
       <p>
-        This is my corner where I post something related to web development!{' '}
-        <br />
-        I&apos;m currently a dedicated file editor with extension{' '}
-        <code>{`js{x},.ts{x}, ...`}</code>, and more
-        <br />I love <code>{`Typescript`}</code> and of course a fan of type
-        safe programming.
+        I am a software engineer with a passion for web development.
+        <br />I love working with Typescript because it allows me to write safer
+        and more maintainable code, and I'm constantly impressed by its ability
+        to catch errors at compile-time. I've worked on a variety of projects
+        that have allowed me to hone my skills in these areas.
       </p>
-      <p></p>
-
-      <h2>Recent Posts</h2>
+      <p>
+        Thanks for stopping by my blog, where I'll be sharing my knowledge and
+        insights about Typescript, System Design, and everything in between!
+      </p>
+      {/* <h2>Recent Posts</h2>
       {posts.map((post) => (
         <PostCard post={post} key={post.slug} />
       ))}
-      <hr />
+      <hr /> */}
 
       {/* <h2>Pet Projects</h2>
       <hr /> */}
